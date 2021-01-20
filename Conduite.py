1ad28de0c7f0a96279517c5cb3006337a2ff54ab
ThierryVuilleum <77577051+ThierryVuilleum@users.noreply.github.com>
2021-01-20 12:05:48 +0100

    Create Conduite.py

---
+++ Conduite.py
@@ -0,0 +1,10 @@
+class Conduite:
+    def __init__(self, Nom, NdDep, NdArr, Diametre, Long, Rugosite, Status):
+        self.Nom = Nom
+        self.NdDep = NdDep
+        self.NdArr = NdArr
+        self.Diametre = Diametre
+        self.Long = Long
+        self.Rugosite = Rugosite
+        self.Status = Status
+        self.Location = []
